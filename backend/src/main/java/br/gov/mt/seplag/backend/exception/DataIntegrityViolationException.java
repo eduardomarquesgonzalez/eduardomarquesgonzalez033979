f76ea7ea7671bfb36ebe06b20cc7a274858b068c
eduardomarquesgonzalez <eduardomarquesgonzalez@gmail.com>
2026-01-30 09:46:50 -0400

    feat(exception): adiciona tratamento global de exceções da API

---
+++ backend/src/main/java/br/gov/mt/seplag/backend/exception/DataIntegrityViolationException.java
@@ -0,0 +1,16 @@
+package br.gov.mt.seplag.backend.exception;
+
+public class DataIntegrityViolationException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public DataIntegrityViolationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public DataIntegrityViolationException(String message) {
+		super(message);
+	}
+
+}
+
+
